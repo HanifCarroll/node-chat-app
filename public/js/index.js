80a3ef9b60cc13d9507d8f612e731c91eab1bc9f
Hanif Carroll <HCarrollUSF@gmail.com>
2017-07-17 18:50:49 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'Jen',
-		text: 'Hey, this is Jen.'
-	});
 });
 
 socket.on('disconnect', function () {
